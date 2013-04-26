zol/meteor

Rewrite bundler and redesign bundle format to support multiple targets. When doing Npm.require on the server, don't infer the node_modules path from the path to the file; read that information out of bundle metadata.

tools/tests/test_bundler_options.js
@@ -10,7 +10,9 @@ var emptyAppDir = path.join(__dirname, 'empty-app');
 /// TESTS
 ///
 
-var lib = new library.Library();
+lib = new library.Library({
+  localPackageDirs: [ path.join(files.getCurrentToolsDir(), 'packages') ]
+});
 
 console.log("nodeModules: 'skip'");
 assert.doesNotThrow(function () {
@@ -20,16 +22,18 @@ assert.doesNotThrow(function () {
 
   // sanity check -- main.js has expected contents.
   assert.strictEqual(fs.readFileSync(path.join(tmpOutputDir, "main.js"), "utf8").trim(),
-                     "require('./server/server.js');");
+                     "require('./programs/server/server.js');");
   // no top level node_modules directory
-  assert(!fs.existsSync(path.join(tmpOutputDir, "server", "node_modules")));
+  assert(!fs.existsSync(path.join(tmpOutputDir,
+                                  "programs", "server", "node_modules")));
   // yes package node_modules directory
   assert(fs.lstatSync(path.join(
-    tmpOutputDir, "npm", "stream"))
+    tmpOutputDir, "programs", "server", "npm", "stream"))
          .isDirectory());
 
   // verify that contents are minified
-  var appHtml = fs.readFileSync(path.join(tmpOutputDir, "app.html"));
+  var appHtml = fs.readFileSync(path.join(tmpOutputDir, "programs",
+                                          "client", "app.html"));
   assert(/src=\"\/[0-9a-f]{40,40}.js\"/.test(appHtml));
   assert(!(/src=\"\/packages/.test(appHtml)));
 });
@@ -42,14 +46,15 @@ assert.doesNotThrow(function () {
 
   // sanity check -- main.js has expected contents.
   assert.strictEqual(fs.readFileSync(path.join(tmpOutputDir, "main.js"), "utf8").trim(),
-                     "require('./server/server.js');");
+                     "require('./programs/server/server.js');");
   // verify that contents are not minified
-  var appHtml = fs.readFileSync(path.join(tmpOutputDir, "app.html"));
+  var appHtml = fs.readFileSync(path.join(tmpOutputDir, "programs",
+                                          "client", "app.html"));
   assert(!(/src=\"\/[0-9a-f]{40,40}.js\"/.test(appHtml)));
   assert(/src=\"\/packages\/meteor/.test(appHtml));
   assert(/src=\"\/packages\/deps/.test(appHtml));
   // verify that tests aren't included
-  assert(!(/src=\"\/packages\/meteor\/url_tests.js/.test(appHtml)));
+  assert(!(/src=\"\/package-tests\/meteor/.test(appHtml)));
 });
 
 console.log("nodeModules: 'skip', no minify, testPackages: ['meteor']");
@@ -61,9 +66,10 @@ assert.doesNotThrow(function () {
 
   // sanity check -- main.js has expected contents.
   assert.strictEqual(fs.readFileSync(path.join(tmpOutputDir, "main.js"), "utf8").trim(),
-                     "require('./server/server.js');");
+                     "require('./programs/server/server.js');");
   // verify that tests for the meteor package are included
-  var appHtml = fs.readFileSync(path.join(tmpOutputDir, "app.html"));
+  var appHtml = fs.readFileSync(path.join(tmpOutputDir, "programs",
+                                          "client", "app.html"));
   assert(/src=\"\/package-tests\/meteor.js/.test(appHtml));
 });
 
@@ -75,11 +81,11 @@ assert.doesNotThrow(function () {
 
   // sanity check -- main.js has expected contents.
   assert.strictEqual(fs.readFileSync(path.join(tmpOutputDir, "main.js"), "utf8").trim(),
-                     "require('./server/server.js');");
+                     "require('./programs/server/server.js');");
   // node_modules directory exists and is not a symlink
-  assert(!fs.lstatSync(path.join(tmpOutputDir, "server", "node_modules")).isSymbolicLink());
+  assert(!fs.lstatSync(path.join(tmpOutputDir, "programs", "server", "node_modules")).isSymbolicLink());
   // node_modules contains fibers
-  assert(fs.existsSync(path.join(tmpOutputDir, "server", "node_modules", "fibers")));
+  assert(fs.existsSync(path.join(tmpOutputDir, "programs", "server", "node_modules", "fibers")));
 });
 
 console.log("nodeModules: 'symlink'");
@@ -90,14 +96,14 @@ assert.doesNotThrow(function () {
 
   // sanity check -- main.js has expected contents.
   assert.strictEqual(fs.readFileSync(path.join(tmpOutputDir, "main.js"), "utf8").trim(),
-                     "require('./server/server.js');");
+                     "require('./programs/server/server.js');");
   // node_modules directory exists and is a symlink
-  assert(fs.lstatSync(path.join(tmpOutputDir, "server", "node_modules")).isSymbolicLink());
+  assert(fs.lstatSync(path.join(tmpOutputDir, "programs", "server", "node_modules")).isSymbolicLink());
   // node_modules contains fibers
-  assert(fs.existsSync(path.join(tmpOutputDir, "server", "node_modules", "fibers")));
+  assert(fs.existsSync(path.join(tmpOutputDir, "programs", "server", "node_modules", "fibers")));
 
   // package node_modules directory also a symlink
   assert(fs.lstatSync(path.join(
-    tmpOutputDir, "npm", "stream"))
+    tmpOutputDir, "programs", "server", "npm", "stream"))
          .isSymbolicLink());
 });

timbrandin/accounts-linkedin

tools/tests/test_bundler_options.js
@@ -10,7 +10,9 @@ var emptyAppDir = path.join(__dirname, 'empty-app');
 /// TESTS
 ///
 
-var lib = new library.Library();
+lib = new library.Library({
+  localPackageDirs: [ path.join(files.getCurrentToolsDir(), 'packages') ]
+});
 
 console.log("nodeModules: 'skip'");
 assert.doesNotThrow(function () {
@@ -20,16 +22,18 @@ assert.doesNotThrow(function () {
 
   // sanity check -- main.js has expected contents.
   assert.strictEqual(fs.readFileSync(path.join(tmpOutputDir, "main.js"), "utf8").trim(),
-                     "require('./server/server.js');");
+                     "require('./programs/server/server.js');");
   // no top level node_modules directory
-  assert(!fs.existsSync(path.join(tmpOutputDir, "server", "node_modules")));
+  assert(!fs.existsSync(path.join(tmpOutputDir,
+                                  "programs", "server", "node_modules")));
   // yes package node_modules directory
   assert(fs.lstatSync(path.join(
-    tmpOutputDir, "npm", "livedata"))
+    tmpOutputDir, "programs", "server", "npm", "livedata"))
          .isDirectory());
 
   // verify that contents are minified
-  var appHtml = fs.readFileSync(path.join(tmpOutputDir, "app.html"));
+  var appHtml = fs.readFileSync(path.join(tmpOutputDir, "programs",
+                                          "client", "app.html"));
   assert(/src=\"\/[0-9a-f]{40,40}.js\"/.test(appHtml));
   assert(!(/src=\"\/packages/.test(appHtml)));
 });
@@ -42,14 +46,15 @@ assert.doesNotThrow(function () {
 
   // sanity check -- main.js has expected contents.
   assert.strictEqual(fs.readFileSync(path.join(tmpOutputDir, "main.js"), "utf8").trim(),
-                     "require('./server/server.js');");
+                     "require('./programs/server/server.js');");
   // verify that contents are not minified
-  var appHtml = fs.readFileSync(path.join(tmpOutputDir, "app.html"));
+  var appHtml = fs.readFileSync(path.join(tmpOutputDir, "programs",
+                                          "client", "app.html"));
   assert(!(/src=\"\/[0-9a-f]{40,40}.js\"/.test(appHtml)));
   assert(/src=\"\/packages\/meteor/.test(appHtml));
   assert(/src=\"\/packages\/deps/.test(appHtml));
   // verify that tests aren't included
-  assert(!(/src=\"\/packages\/meteor\/url_tests.js/.test(appHtml)));
+  assert(!(/src=\"\/package-tests\/meteor/.test(appHtml)));
 });
 
 console.log("nodeModules: 'skip', no minify, testPackages: ['meteor']");
@@ -61,9 +66,10 @@ assert.doesNotThrow(function () {
 
   // sanity check -- main.js has expected contents.
   assert.strictEqual(fs.readFileSync(path.join(tmpOutputDir, "main.js"), "utf8").trim(),
-                     "require('./server/server.js');");
+                     "require('./programs/server/server.js');");
   // verify that tests for the meteor package are included
-  var appHtml = fs.readFileSync(path.join(tmpOutputDir, "app.html"));
+  var appHtml = fs.readFileSync(path.join(tmpOutputDir, "programs",
+                                          "client", "app.html"));
   assert(/src=\"\/package-tests\/meteor.js/.test(appHtml));
 });
 
@@ -75,11 +81,11 @@ assert.doesNotThrow(function () {
 
   // sanity check -- main.js has expected contents.
   assert.strictEqual(fs.readFileSync(path.join(tmpOutputDir, "main.js"), "utf8").trim(),
-                     "require('./server/server.js');");
+                     "require('./programs/server/server.js');");
   // node_modules directory exists and is not a symlink
-  assert(!fs.lstatSync(path.join(tmpOutputDir, "server", "node_modules")).isSymbolicLink());
+  assert(!fs.lstatSync(path.join(tmpOutputDir, "programs", "server", "node_modules")).isSymbolicLink());
   // node_modules contains fibers
-  assert(fs.existsSync(path.join(tmpOutputDir, "server", "node_modules", "fibers")));
+  assert(fs.existsSync(path.join(tmpOutputDir, "programs", "server", "node_modules", "fibers")));
 });
 
 console.log("nodeModules: 'symlink'");
@@ -90,14 +96,14 @@ assert.doesNotThrow(function () {
 
   // sanity check -- main.js has expected contents.
   assert.strictEqual(fs.readFileSync(path.join(tmpOutputDir, "main.js"), "utf8").trim(),
-                     "require('./server/server.js');");
+                     "require('./programs/server/server.js');");
   // node_modules directory exists and is a symlink
-  assert(fs.lstatSync(path.join(tmpOutputDir, "server", "node_modules")).isSymbolicLink());
+  assert(fs.lstatSync(path.join(tmpOutputDir, "programs", "server", "node_modules")).isSymbolicLink());
   // node_modules contains fibers
-  assert(fs.existsSync(path.join(tmpOutputDir, "server", "node_modules", "fibers")));
+  assert(fs.existsSync(path.join(tmpOutputDir, "programs", "server", "node_modules", "fibers")));
 
   // package node_modules directory also a symlink
   assert(fs.lstatSync(path.join(
-    tmpOutputDir, "npm", "livedata"))
+    tmpOutputDir, "programs", "server", "npm", "livedata"))
          .isSymbolicLink());
 });